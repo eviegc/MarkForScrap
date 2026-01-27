8753b0522b59d96b31da46406358dbf1ac300d39
evie <141637929+eviegc@users.noreply.github.com>
2026-01-27 20:38:51 +0000

    update IsScrappable logic to be more robust / allow for scrapping quality items

--- MarkForScrap/Utils/ItemUtils.cs
+++ MarkForScrap/Utils/ItemUtils.cs
@@ -7,26 +7,17 @@ namespace MarkForScrap.Utils
         public static bool IsScrappable(ItemIndex idx)
         {
             ItemDef def = ItemCatalog.GetItemDef(idx);
-            if (def == null)
+            ItemTierDef tierDef = ItemTierCatalog.GetItemTierDef(def.tier);
+
+            if (
+                !def.canRemove
+                || !tierDef.canScrap
+                || def.ContainsTag(ItemTag.Scrap)
+                || def.ContainsTag(ItemTag.ObjectiveRelated)
+            )
                 return false;
 
-            if (def.ContainsTag(ItemTag.Scrap))
-                return false;
-            if (def.ContainsTag(ItemTag.WorldUnique))
-                return false;
-            if (def.hidden)
-                return false;
-
-            switch (def.tier)
-            {
-                case ItemTier.Tier1:
-                case ItemTier.Tier2:
-                case ItemTier.Tier3:
-                case ItemTier.Boss:
-                    return true;
-                default:
-                    return false;
-            }
+            return true;
         }
     }
 }
